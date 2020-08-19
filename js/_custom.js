4fe8e32591026c2ef4e0d430f69b162ef1f22002
Maksim Koshman <maksimkoshman090@gmail.com>
2020-08-19 11:42:23 +0300

    create project

---
+++ js/_custom.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  // Close tablet menu
+  $(".subheader__burger__hidden").on("click", function () {
+    $(".subheader__nav__hidden").removeClass("active");
+    $(".subheader__burger").removeClass("active");
+    $("html").removeClass("overflow");
+  });
+
+  // Burger menu open/close
+  $(".subheader__burger").on("click", function () {
+    $(".subheader__nav__hidden").toggleClass("active");
+    $(".subheader__burger").toggleClass("active");
+    $("html").toggleClass("overflow");
+  });
+
+  // обработчик события resize
+  $(window).resize(function () {
+    if ($(window).width() <= "1199") {
+      // Intro Slider
+      $("#introSlider")
+        .not(".slick-initialized")
+        .slick({
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplaySpeed: 2000,
+          nextArrow: $("#introNext"),
+          prevArrow: $("#introPrev"),
+        });
+    }
+
+    if ($(window).width() <= "767") {
+    }
+  });
+
+  // вызовем событие resize
+  $(window).resize();
+});
